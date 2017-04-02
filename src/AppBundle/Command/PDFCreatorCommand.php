55e3ffa6b9fbc93af492f935836e29de8573463a
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-04-02 22:18:41 +0200

    Generating pdfs with command

--- src/AppBundle/Command/PDFCreatorCommand.php
+++ src/AppBundle/Command/PDFCreatorCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use AppBundle\Entity\Post;
+use AppBundle\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -24,10 +25,59 @@ class PDFCreatorCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $post = $this->getContainer()->get('doctrine')
-            ->getRepository(Post::class)
-            ->find('3');
+        $this->generatePosts();
+        $this->generateRecipes();
+    }
 
-        $this->getContainer()->get('app_bundle.pdf')->generate($post);
+    private function generateAbsoluteUrl($offset)
+    {
+        return sprintf("%s%s", $this->getContainer()->getParameter('base_url'), $offset);
+    }
+
+    private function generatePath($offset)
+    {
+        return sprintf("%s/%s.pdf", $this->getContainer()->getParameter('pdf_path'), $offset);
+    }
+
+    private function generatePosts()
+    {
+        /** @var Post[] $posts */
+        $posts = $this->getContainer()->get('doctrine')
+            ->getRepository(Post::class)
+            ->findAll();
+
+        foreach ($posts as $post) {
+            $filePath = $this->generatePath(sprintf("post_%s", $post->getSlug()));
+
+            $url = $this->getContainer()
+                ->get('router')
+                ->generate('print_view_post', ['slug' => $post->getSlug()]);
+
+            $this->getContainer()->get('app_bundle.pdf')->generate(
+                $this->generateAbsoluteUrl($url),
+                $filePath
+            );
+        }
+    }
+
+    private function generateRecipes()
+    {
+        /** @var Recipe[] $posts */
+        $posts = $this->getContainer()->get('doctrine')
+            ->getRepository(Recipe::class)
+            ->findAll();
+
+        foreach ($posts as $post) {
+            $filePath = $this->generatePath(sprintf("recpie_%s", $post->getId()));
+
+            $url = $this->getContainer()
+                ->get('router')
+                ->generate('print_view_recipe', ['id' => $post->getId()]);
+
+            $this->getContainer()->get('app_bundle.pdf')->generate(
+                $this->generateAbsoluteUrl($url),
+                $filePath
+            );
+        }
     }
 }
