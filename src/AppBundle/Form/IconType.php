a3a12d2e9b0ac1c08b4c94d8ebcefb4c65736a6a
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2017-04-03 07:36:08 +0200

    Refactoring

--- src/AppBundle/Form/IconType.php
+++ src/AppBundle/Form/IconType.php
@@ -2,53 +2,38 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class IconType extends AbstractType
 {
-    const CHOICE_MAP = [
-        'images/categories/5_a_day.png',
-        'images/categories/about.png',
-        'images/categories/alcohol.png',
-        'images/categories/all.png',
-        'images/categories/archive.png',
-        'images/categories/baked.png',
-        'images/categories/bread.jpg',
-        'images/categories/breakfast.png',
-        'images/categories/carbs.png',
-        'images/categories/dessert.png',
-        'images/categories/drinks.jpg',
-        'images/categories/fish.png',
-        'images/categories/handmade.png',
-        'images/categories/hauptgang.jpg',
-        'images/categories/high_protein.png',
-        'images/categories/home.png',
-        'images/categories/icons8.png',
-        'images/categories/ingredients.png',
-        'images/categories/lowcarb.png',
-        'images/categories/meat.png',
-        'images/categories/no_alcohol.png',
-        'images/categories/no_gluten.png',
-        'images/categories/no_milk.png',
-        'images/categories/no_sugar.png',
-        'images/categories/pdf.png',
-        'images/categories/pizza.jpg',
-        'images/categories/salad.png',
-        'images/categories/scharfes.jpg',
-        'images/categories/smoothie.jpg',
-        'images/categories/snack.png',
-        'images/categories/sosse.png',
-        'images/categories/soup.png',
-        'images/categories/vegan.png',
-        'images/categories/vegetarian.png',
-    ];
+    private $categoryPath;
+    private $categoryOffset;
+
+    public function __construct($webPath, $categoryOffset)
+    {
+        $this->categoryPath = sprintf("%s%s", $webPath, $categoryOffset);
+        $this->categoryOffset = $categoryOffset;
+    }
+
+    private function findIcons()
+    {
+        $finder = new Finder();
+
+        $files = [];
+        foreach ($finder->in($this->categoryPath) as $file) {
+            $files[] = sprintf("%s/%s", $this->categoryOffset, $file->getRelativePathname());
+        }
+        return $files;
+    }
 
     public function configureOptions(OptionsResolver $resolver)
     {
+        $paths = $this->findIcons();
         $resolver->setDefaults([
-            'choices' => array_combine(self::CHOICE_MAP, self::CHOICE_MAP)
+            'choices' => array_combine($paths, $paths)
         ]);
     }
 
--- src/AppBundle/Service/PDFService.php
+++ src/AppBundle/Service/PDFService.php
@@ -4,20 +4,46 @@ namespace AppBundle\Service;
 
 use AppBundle\Entity\Post;
 use JonnyW\PhantomJs\Client;
+use JonnyW\PhantomJs\Http\PdfRequest;
 use PhantomInstaller\PhantomBinary;
 use Symfony\Component\Routing\Router;
 
 class PDFService
 {
+    /**
+     * @param string $url
+     * @param string $outputFile
+     * @param bool $overwrite
+     * @return void
+     */
     public function generate($url, $outputFile, $overwrite = false)
     {
         if (!$overwrite && file_exists($outputFile)) {
-            return $outputFile;
+            return;
         }
 
         $client = Client::getInstance();
         $client->getEngine()->setPath(PhantomBinary::getBin());
 
+        $request = $this->createParametrizedRequest($url, $outputFile, $client);
+
+        $response = $client->getMessageFactory()->createResponse();
+        $client->send($request, $response);
+
+        if ($response->getStatus() != 200) {
+            dump($request, $response);
+            throw new \RuntimeException("PDF could not be generated");
+        }
+    }
+
+    /**
+     * @param string $url
+     * @param string $outputFile
+     * @param Client $client
+     * @return PdfRequest
+     */
+    private function createParametrizedRequest($url, $outputFile, Client $client)
+    {
         $request = $client->getMessageFactory()->createPdfRequest($url);
 
         $request->setOutputFile($outputFile);
@@ -25,12 +51,8 @@ class PDFService
         $request->setOrientation('portrait');
         $request->setMargin('1cm');
         $request->setCaptureDimensions(720, 0);
+        $request->setTimeout(15000);
 
-        $response = $client->getMessageFactory()->createResponse();
-        $client->send($request, $response);
-
-        if ($response->getStatus() != 200) {
-            throw new \RuntimeException("PDF could not be generated");
-        }
+        return $request;
     }
 }
